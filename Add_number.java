d42f8c21417af2598ece249a19daa9f996b578a4
mrsajar <mssaifi386@gmail.com>
2022-04-24 04:43:23 -0700

    Add two number

---
+++ Add_number.java
@@ -0,0 +1,13 @@
+class Addition{
+        public int find_add(int a, int b){
+            int c=0;
+           return c=a+b;
+        }
+    }
+    public class Add_number {
+    public static void main(String[] args) {
+        Addition obj=new Addition();
+        System.out.println(obj.find_add(12,12));
+
+    }
+}
