70ca24558f8671d95ad5595b9cd08557fe5d599a
mrsajar <mssaifi386@gmail.com>
2023-01-25 09:06:26 +0530

    making add

--- JavaStream.java
+++ JavaStream.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class JavaStream {
     public  static  void  main(String[] args){
         System.out.println("shah");
-        System.out.println("sajar");
+        System.out.println("Sajar");
+        System.out.println("making add new field");
     }
 
 
