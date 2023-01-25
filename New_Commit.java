6597f9fb68a84fdb0489de7456f57eb67292a5d7
mrsajar <mssaifi386@gmail.com>
2023-01-25 09:00:41 +0530

    new add

--- New_Commit.java
+++ New_Commit.java
@@ -1,5 +1,6 @@
 public class New_Commit {
     public static void main(String[] args) {
         System.out.println("first commit of branch");
+        System.out.println("we can use multiple command using git");
     }
 }
