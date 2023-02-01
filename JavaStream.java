69a97b4e0faf50ee6947bb5fd714d542470a5d99
mrsajar <mssaifi386@gmail.com>
2023-02-01 23:18:53 +0530

    sorting array by git

--- JavaStream.java
+++ JavaStream.java
@@ -1,12 +1,25 @@
 public class JavaStream {
     public  static  void  main(String[] args){
-        int a=10;
-        for (int i=1;i<=a;i++){
+        int arr[]={2,5,7,5,34,32,};
 
-            System.out.println(i);
+        for(int i=0;i<arr.length;i++){
+
+            for (int j=i+1;j< arr.length;j++){
+
+                if (arr[i]>arr[j]){
+
+                    int local=arr[i];
+
+                    arr[i]=arr[j];
+
+                    arr[j]=local;
+                }
+            }
+        }
+        for (int i=0;i< arr.length;i++){
+            System.out.println(arr[i]);
         }
 
     }
-
 
 }
