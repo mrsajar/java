75ee3e5dd14048c78ce2c97816d7f8e1e05a4e29
mrsajar <mssaifi386@gmail.com>
2023-05-06 19:07:13 +0530

    this file for  java 8

--- PracticeTask/Data.java
+++ PracticeTask/Data.java
@@ -2,6 +2,6 @@ package PracticeTask;
 
 public class Data {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("this data class use for java 8");
     }
 }
